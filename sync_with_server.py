84cb1f88ea2a31a96b3a6a0d83a55468555a1aef
Octavio <octmb.94@gmail.com>
2018-06-19 16:57:50 +0200

    Update sync_with_server

--- sync_with_server.py
+++ sync_with_server.py
@@ -13,7 +13,7 @@ parser.add_argument('username', metavar='SSH username', type=str,
 parser.add_argument('project_path', metavar='Project path directory', type=str,
                     help='Path for the directory of the project')
 # password, we can change this if we want to put a default password
-parser.add_argument('--password', '-p', type='str', help='Password for ssh auth if no keys are configured',
+parser.add_argument('--password', '-p', type=str, help='Password for ssh auth if no keys are configured',
                     default=None)
 
 parser.add_argument('--sync_rate', '-s', type=int, help='Syncing rate (seconds)',
@@ -30,9 +30,9 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # if no password is configured then use the key
 if args.password is None:
-    ssh_client.connect(hostname=HOSTNAME, username=args.username)
+    ssh_client.connect(hostname=HOSTNAME, username=args.username, compress=True)
 else:
-    ssh_client.connect(hostname=HOSTNAME, username=args.username, password=args.password)
+    ssh_client.connect(hostname=HOSTNAME, username=args.username, password=args.password, compress=True)
 
 
 output_model_dir = os.path.join(args.project_path, 'output', args.model_name)
@@ -43,8 +43,13 @@ mirror_dir = os.path.join(os.path.curdir, 'mirrored')
 if not os.path.exists(mirror_dir):
     os.makedirs(mirror_dir)
 
+def get_seconds():
+    return int(round(time.time()))
+
 while True:
     print('Syncing...')
-    scp.get(output_model_dir, mirror_dir)
-    print('...finished syncing')
+    start_time = get_seconds()
+    scp.get(output_model_dir, mirror_dir, recursive=True, preserve_times=True)
+    end_time = get_seconds()
+    print(f'...finished syncing in {end_time - start_time} s')
     time.sleep(args.sync_rate)
