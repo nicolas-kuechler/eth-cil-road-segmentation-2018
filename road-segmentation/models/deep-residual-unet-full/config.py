a74824b9e9b788ef8241c9d9dc8310c2e6f33eaa
Nicolas Küchler <nico.kuechler@gmail.com>
2018-06-21 10:47:46 +0200

    add a variation of the deep residual unet using full patches

--- road-segmentation/models/deep-residual-unet-full/config.py
+++ road-segmentation/models/deep-residual-unet-full/config.py
@@ -7,11 +7,6 @@ class Config(AbstractConfig):
         super().__init__(model_name)
 
     # Overwrite any Configurations from Abstract Config
-    TRAIN_METHOD_PATCH_SIZE_PERCENTAGE = 0.5
-
-    VALID_METHOD_PATCH_SIZE = 200
-    VALID_METHOD_STRIDE = 200
-
     N_EPOCHS = 50
     N_BATCHES_PER_EPOCH = 1000
     TRAIN_BATCH_SIZE = 8
@@ -26,5 +21,11 @@ class Config(AbstractConfig):
     USE_GRADIENT_CLIPPING = False
 
     SUMMARY_IMAGE_EVERY_STEP = 500
+
+    VALID_METHOD_NAME = 'full'
+    TRAIN_METHOD_NAME = 'full'
+
+    AUG_FLIP_RANDOM_PROB = 0.000000000000001
+    AUG_ZOOM_RANDOM_PERCENTAGE_AREA = 0.95
 
     # Define new Configurations for your Model
