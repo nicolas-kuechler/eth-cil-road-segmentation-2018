53213b2e1c54aa58f368d11f2a83ffd2fa67d7f3
Octavio <octmb.94@gmail.com>
2018-06-21 22:17:46 +0200

    Add initial squeezenet model

--- road-segmentation/models/simple-autoencoder/config.py
+++ road-segmentation/models/simple-autoencoder/config.py
@@ -12,3 +12,10 @@ class Config(AbstractConfig):
     TRAIN_METHOD_NAME = 'full' # patch or full
 
     # Define new Configurations for your Model
+
+    N_fire_modules = 8
+    FILTERS_SQUEEZE1 =  [16, 16, 32, 32, 48, 48, 64, 64]
+    FILTERS_EXPAND1 =   [64, 64, 128, 128, 192, 192, 256, 256]
+    FILTERS_EXPAND3 =   [64, 64, 128, 128, 192, 192, 256, 256]
+    DROPOUTS =  [0, 0, 0, 0, 0, 0, 0, 1]
+    MAX_POOLS = [1, 0, 0, 1, 0, 0, 0, 1]
--- road-segmentation/models/simple-autoencoder/model.py
+++ road-segmentation/models/simple-autoencoder/model.py
@@ -8,38 +8,81 @@ class Model(AbstractModel):
         super().__init__(config, dataset, mode)
 
     def build_model(self):
+        self.check_configs()
         self.images = self.dataset.img_batch
         self.labels = self.dataset.labels
 
         # Build Neural Network Architecture (here single convolution layer)
         input = tf.cast(self.images, tf.float32)
 
-        # Squeeze
-        with tf.name_scope(name='Squeeze'):
-            conv1 = tf.layers.conv2d(inputs=input, filters=64, kernel_size=1, padding='same', activation=tf.nn.relu,
-                                     name='squeeze_1')
+        # first conv layer
+        input = tf.layers.conv2d(inputs=input, filters=96, kernel_size=7, strides=2,
+                                 padding='same', name='conv1')
 
+        # fire modules
+        for layer_number in range(self.config.N_fire_modules):
+            input = self.fire_module(input, layer_number)
 
+        # inverted fire modules
+        for layer_number in range(self.config.N_fire_modules):
+            input = self.inverse_fire_module(input, layer_number)
+
+        # final conv layer
+        self.predictions = tf.layers.conv2d_transpose(inputs=input, filters=1, kernel_size=7, strides=2,
+                                 padding='same', name='final_conv', activation=tf.nn.relu)
 
         # Define predictions, train_op, loss
-        self.predictions = conv
         self.loss = tf.losses.mean_squared_error(self.labels, self.predictions)
 
         # optimize
         self.optimize()
 
-    def fire_module(self, input, number):
-        # n filters in squeeze 1x1 should be smaller than the total of filters in expand 1x1 + expand 3x3 c.f. paper
-        assert(self.config.N_FILTERS_SQUEEZE[number] <=
-               (self.config.N_FILTERS_EXPAND1[number] + self.config.N_FILTERS_EXPAND3[number]))
-        with tf.name_scope(name='fire_module' + str(number)):
+    def fire_module(self, input, layer_number):
+        if self.config.MAX_POOLS[layer_number]:
+            with tf.name_scope(name='maxpool' + str(layer_number)):
+                input = tf.layers.max_pooling2d(inputs=input, pool_size=3, strides=2, padding='same')
+
+        with tf.name_scope(name='fire_module' + str(layer_number)):
             with tf.name_scope(name='squeeze'):
-                input = tf.layers.conv2d(inputs=input, filters=self.config.N_FILTERS_SQUEEZE[number],
-                                         kernel_size=1, strides=1, activation=tf.nn.relu)
+                input = tf.layers.conv2d(inputs=input, filters=self.config.FILTERS_SQUEEZE1[layer_number],
+                                         kernel_size=1, strides=1, activation=tf.nn.relu, padding='same')
             with tf.name_scope(name='expand'):
-                input = tf.layers.conv2d(inputs=input, filters=self.config.N_FILTERS_EXPAND1[number],
-                                         kernel_size=1, strides=1)
-                input = tf.layers.conv2d(inputs=input, filters=self.config.N_FILTERS_EXPAND3[number],
-                                         kernel_size=3, strides=1)
+                input = tf.layers.conv2d(inputs=input, filters=self.config.FILTERS_EXPAND1[layer_number],
+                                         kernel_size=1, strides=1, padding='same')
+                input = tf.layers.conv2d(inputs=input, filters=self.config.FILTERS_EXPAND3[layer_number],
+                                         kernel_size=3, strides=1, padding='same')
                 input = tf.nn.relu(input)
+
+        if self.config.DROPOUTS[layer_number]:
+            with tf.name_scope(name='dropout' + str(layer_number)):
+                input = tf.nn.dropout(input, keep_prob=0.5)
+
         return input
+
+    def inverse_fire_module(self, input, layer_number):
+        with tf.name_scope(name='inverse_fire_module' + str(layer_number)):
+            with tf.name_scope(name='expand'):
+                input = tf.layers.conv2d(inputs=input, filters=self.config.FILTERS_EXPAND3[-layer_number-1],
+                                         kernel_size=3, strides=1, padding='same')
+                input = tf.layers.conv2d(inputs=input, filters=self.config.FILTERS_EXPAND1[-layer_number-1],
+                                         kernel_size=1, strides=1, padding='same')
+                input = tf.nn.relu(input)
+            with tf.name_scope(name='squeeze'):
+                input = tf.layers.conv2d(inputs=input, filters=self.config.FILTERS_SQUEEZE1[-layer_number-1],
+                                         kernel_size=1, strides=1, activation=tf.nn.relu, padding='same')
+
+        if self.config.MAX_POOLS[-layer_number-1]:
+            with tf.name_scope(name='maxpool' + str(layer_number)):
+                input = tf.layers.conv2d_transpose(inputs=input, filters=self.config.FILTERS_SQUEEZE1[-layer_number-1],
+                                                   kernel_size=3, strides=2, padding='same')
+        return input
+
+    def check_configs(self):
+        assert (self.config.N_fire_modules == len(self.config.FILTERS_SQUEEZE1))
+        assert (self.config.N_fire_modules == len(self.config.FILTERS_EXPAND1))
+        assert (self.config.N_fire_modules == len(self.config.FILTERS_EXPAND3))
+
+        # n filters in squeeze 1x1 should be smaller than the total of filters in expand 1x1 + expand 3x3 c.f. paper
+        for layer_number in range(self.config.N_fire_modules):
+            assert (self.config.FILTERS_SQUEEZE1[layer_number] <=
+                    (self.config.FILTERS_EXPAND1[layer_number] + self.config.FILTERS_EXPAND3[layer_number]))
