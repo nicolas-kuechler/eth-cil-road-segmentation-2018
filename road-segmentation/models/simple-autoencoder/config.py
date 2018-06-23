c763bca373ec50e93283735433fa00f253bef623
Octavio <octmb.94@gmail.com>
2018-06-23 02:27:58 +0200

    Update config file for training

--- road-segmentation/models/simple-autoencoder/config.py
+++ road-segmentation/models/simple-autoencoder/config.py
@@ -7,12 +7,15 @@ class Config(AbstractConfig):
 
 
     # Overwrite any Configurations from Abstract Config
-    N_EPOCHS = 100
-    N_BATCHES_PER_EPOCH = 4
-    LEARNING_RATE = 0.0007
+    N_EPOCHS = 10000
+    N_BATCHES_PER_EPOCH = 100
+    LEARNING_RATE = 0.0001
     TRAIN_METHOD_NAME = 'full' # patch or full
     VALID_METHOD_NAME = 'full'
-    SUMMARY_IMAGE_EVERY_STEP = 10
+    SUMMARY_IMAGE_EVERY_STEP = 400
+    TRAIN_BATCH_SIZE = 32
+    SUMMARY_IMAGE_MAX_OUTPUTS = 3
+    SUMMARY_FULL_IMAGE_MAX_OUTPUTS = 3
 
     # Define new Configurations for your Model
 
