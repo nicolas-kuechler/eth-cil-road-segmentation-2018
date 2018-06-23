8f2b747856f3402ca3c224a5d860e9f070e3dccb
Octavio <octmb.94@gmail.com>
2018-06-23 22:22:13 +0200

    Update the evaluation

--- road-segmentation/core/evaluation.py
+++ road-segmentation/core/evaluation.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 from utility import image_split_and_merge as isam
+import os
 
 class Evaluation():
 
@@ -42,6 +43,16 @@ class Evaluation():
         labels = np.concatenate(labels, axis=0)
         predictions = np.concatenate(predictions, axis=0)
 
+        # We start with a new submission file, delete previous one if it exists
+        submission_file = self.config.TEST_OUTPUT_DIR + 'submission.csv'
+        try:
+            os.remove(submission_file)
+        except OSError:
+            pass
+        with open(submission_file, 'a') as f:
+            f.write('id,prediction\n')
+
+
         if self.config.TEST_METHOD_NAME == 'patch':
             # if patch based -> put images back together
 
@@ -57,10 +68,11 @@ class Evaluation():
                                                 image_size=(self.config.TEST_IMAGE_SIZE, self.config.TEST_IMAGE_SIZE, 1),
                                                 stride=self.config.TEST_METHOD_STRIDE)
                 img_id = int(labels[start, 0])
+                self.append_prediction(submission_file, img[:, :, 0], img_id)
+
 
                 # scale back
                 img = img * 255
-
                 img = Image.fromarray(img[:,:,0].astype('uint8'))
                 img.save(self.config.TEST_OUTPUT_DIR + 'out{}.png'.format(img_id))
 
@@ -69,11 +81,34 @@ class Evaluation():
 
             for i in range(n_images):
                 img = predictions[i, :, :, 0]
+                img_id = int(labels[i])
+                self.append_prediction(submission_file, img, img_id)
                 img = img * 255
                 img = Image.fromarray(img.astype('uint8'))
-                img_id = int(labels[i])
                 img.save(self.config.TEST_OUTPUT_DIR + 'out{}.png'.format(img_id))
         else:
             raise ValueError('Unknown Test Method Name')
 
-        print('Evaluation Finished: saved {} masks to {}'.format(n_images, self.config.TEST_OUTPUT_DIR))
+        print('Evaluation Finish   ed: saved {} masks to {}'.format(n_images, self.config.TEST_OUTPUT_DIR))
+
+    def append_prediction(self, submission_file, prediction, id):
+        with open(submission_file, 'a') as f:
+            f.writelines('{}\n'.format(s) for s in self.prediction_to_submission(prediction, id))
+
+    def prediction_to_submission(self, prediction, id):
+        patch_size = 16
+        for j in range(0, prediction.shape[1], patch_size):
+            for i in range(0, prediction.shape[0], patch_size):
+                patch = prediction[i:i + patch_size, j:j + patch_size]
+                label = self.patch_to_label(patch)
+                yield ("{:03d}_{}_{},{}".format(id, j, i, label))
+
+
+    # assign a label to a patch
+    def patch_to_label(self, patch):
+        foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
+        df = np.mean(patch)
+        if df > foreground_threshold:
+            return 1
+        else:
+            return 0
