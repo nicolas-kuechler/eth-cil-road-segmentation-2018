b2b3c2f3583e20b74c0622d945dfa3a8c19a0ec6
Octavio <octmb.94@gmail.com>
2018-06-23 02:35:24 +0200

    Run for 30 epochs

--- road-segmentation/models/simple-autoencoder/config.py
+++ road-segmentation/models/simple-autoencoder/config.py
@@ -7,7 +7,7 @@ class Config(AbstractConfig):
 
 
     # Overwrite any Configurations from Abstract Config
-    N_EPOCHS = 10000
+    N_EPOCHS = 30
     N_BATCHES_PER_EPOCH = 100
     LEARNING_RATE = 0.0001
     TRAIN_METHOD_NAME = 'full' # patch or full
