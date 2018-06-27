49a34d104df437ec517f9a0179151e5ebd4cb415
Octavio <octmb.94@gmail.com>
2018-06-27 21:13:21 +0200

    Adapt models

--- road-segmentation/models/squeezenet-encoder-normalization/config.py
+++ road-segmentation/models/squeezenet-encoder-normalization/config.py
@@ -2,8 +2,8 @@ from core.abstract_config import AbstractConfig
 
 class Config(AbstractConfig):
 
-    def __init__(self, model_name: str):
-        super().__init__(model_name)
+    def __init__(self, model_name: str, ext):
+        super().__init__(model_name, ext)
 
 
     # Overwrite any Configurations from Abstract Config
@@ -26,3 +26,7 @@ class Config(AbstractConfig):
     FILTERS_EXPAND3 =   [64, 64, 128, 128, 192, 192, 256, 256]
     DROPOUTS =  [0, 0, 0, 0, 0, 0, 0, 1]
     MAX_POOLS = [1, 0, 0, 1, 0, 0, 0, 1]
+
+    AUG_GAUSSIAN_BLUR_PROB = 0.00001
+    AUG_GAUSSIAN_BLUR_MIN_SIGMA = 0.01
+    AUG_GAUSSIAN_BLUR_MAX_SIGMA = 5
