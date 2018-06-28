df4ebbe8572da6fcddb218a399cb79edcd9b2311
Sabina Fischlin <fsabina@ethz.ch>
2018-06-28 18:18:26 -0500

    postprocessing

--- road-segmentation/postprocessing/postprocessing_config.py
+++ road-segmentation/postprocessing/postprocessing_config.py
@@ -6,18 +6,11 @@ class Config(AbstractConfig):
     def __init__(self, batch_name):
         super().__init__( batch_name, '' )
         
-        self.PREDICTIONS_IN_DIR = self.POST_DIR + 'to_process/' + batch_name
+        self.POST_PREDICTIONS_IN_DIR = self.BASE_DIR + 'output/' + batch_name + '/test_output/predictions/'
         self.TEST_IMAGES_IN_DIR = './../data/test_images/'
         
-        self.OUTPUT_DIR = self.POST_DIR + 'output/' + batch_name + '/'
-        
-        self.WRITE_SUBMISSION = True
-        
+        self.OUTPUT_DIR = self.BASE_DIR + 'output/' + batch_name + '_post/'
+                
 		# Overwrite any Configurations from Abstract Config
-        self.TEST_OUTPUT_DIR = self.OUTPUT_DIR + 'submission/' # only used for submissions
+        self.TEST_OUTPUT_DIR = self.OUTPUT_DIR
         self.TEST_PATH_TO_DATA = self.TEST_IMAGES_IN_DIR
-        
-        #self.MAX_NUM_PROCESSES = 2
-        self.MAX_NUM_PROCESSES = sys.maxsize
-        
-    POST_DIR = './postprocessing/'
