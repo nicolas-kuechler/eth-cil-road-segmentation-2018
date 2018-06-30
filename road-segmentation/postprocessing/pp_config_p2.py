49b79689e428e046d82d5c38f3f70ba2dd4fb09c
Sabina Fischlin <fsabina@ethz.ch>
2018-06-30 00:45:39 -0500

    added parameter set configuration files
    Former-commit-id: 38d406f4ef2badd1a0fda7cf1cc3862bd1a482a8 [formerly f5778ac4c7ccf2e95c2f8a6eb9b9f86212509a28]
    Former-commit-id: 5cb09f4e65e7a353975e90a0ace715f8587edc96

---
+++ road-segmentation/postprocessing/pp_config_p2.py
@@ -0,0 +1,28 @@
+from core.abstract_config import AbstractConfig
+
+class Config(AbstractConfig):
+
+    def __init__(self, batch_name):
+        super().__init__( batch_name, '' )
+        
+        self.POST_PREDICTIONS_IN_DIR = self.BASE_DIR + 'output/' + batch_name + '/test_output/predictions/'
+        self.TEST_IMAGES_IN_DIR = './../data/test_images/'
+        
+        self.OUTPUT_DIR = self.BASE_DIR + 'output/' + batch_name + '_post_p2/test_output/'
+                
+		# Overwrite any Configurations from Abstract Config
+        self.TEST_OUTPUT_DIR = self.OUTPUT_DIR
+        self.TEST_PATH_TO_DATA = self.TEST_IMAGES_IN_DIR
+
+    POST_NUM_INFERENCE_IT = 50
+    
+    POST_SDIMS_GAUSSIAN_X = 0.05
+    POST_SDIMS_GAUSSIAN_Y = 0.05
+    POST_COMPAT_GAUSSIAN = 15
+    
+    POST_SDIMS_BILATERAL_X = 120
+    POST_SDIMS_BILATERAL_Y = 120
+    POST_SCHAN_BILATERAL_R = 40
+    POST_SCHAN_BILATERAL_G = 40
+    POST_SCHAN_BILATERAL_B = 40
+    POST_COMPAT_BILATERAL = 8
