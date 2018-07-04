6e41e2f4efa609b5141172a443907056491e720e
Octavio <octmb.94@gmail.com>
2018-07-05 01:11:39 +0200

    Update outputs and bash script
    Former-commit-id: 82baa99a2744b770a225de434bef051cd2bba17b [formerly 3657a7a71fa5459a582d778d4be9bff1ec78a87c]
    Former-commit-id: 2e5e1cfe1159982e683d406c9c2cee0f3b425ff7

--- create_submission.sh
+++ create_submission.sh
@@ -2,13 +2,13 @@
 
 # vanilla-unet training full dataset
 echo "Starting to train the vanilla unet with full extended dataset"
-python main.py vanilla-unet test ext-full
+python main.py vanilla-unet train ext-full
 
 echo "Producing predictions for vanilla unet"
 python main.py vanilla-unet test ext-full
 
 echo "Postprocessing the vanilla unet results"
-python main_postprocessing.py vanilla-unet_ext p1
+python main_postprocessing.py vanilla-unet_ext-full p1
 
 
 # squeezenet-encoder
@@ -20,4 +20,4 @@ python main_postprocessing.py squeezenet-encoder-dropouts_ext-half p1
 
 # average
 echo "Averaging post-processed results from both networks"
-python average.py squeezenet-encoder-dropouts_ext-half_post_p1 vanilla-unet_ext_post_p1
+python average.py squeezenet-encoder-dropouts_ext-half_post_p1 vanilla-unet_ext-full_post_p1
